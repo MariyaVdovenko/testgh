a81d6d5a29f10c80f84106ed5a872286d905cf4e
MariyaVdovenko <m.vdovenko@ukr.net>
2019-12-01 00:25:11 +0200

    Updates

---
+++ precache-manifest.1dd16812637a33161f518255bf0b179c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdf02764edabbe7fc215d1b4adfbec8",
+    "url": "/testgh/index.html"
+  },
+  {
+    "revision": "00927becd05606a98a45",
+    "url": "/testgh/static/css/main.7f9cd39a.chunk.css"
+  },
+  {
+    "revision": "b40bf4929657dbff27fc",
+    "url": "/testgh/static/js/2.7066c695.chunk.js"
+  },
+  {
+    "revision": "00927becd05606a98a45",
+    "url": "/testgh/static/js/main.ba052de0.chunk.js"
+  },
+  {
+    "revision": "d1255e8def2c7cd35fd1",
+    "url": "/testgh/static/js/runtime-main.512216cf.js"
+  }
+]);
